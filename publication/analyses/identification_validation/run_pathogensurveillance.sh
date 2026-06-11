cba51d73c45c768b24445e9241b756ea2a0beaf2
Zachary Foster <zacharyfoster1989@gmail.com>
2026-06-10 18:57:48 -0700

    refine sankey plot

--- publication/analyses/identification_validation/run_pathogensurveillance.sh
+++ publication/analyses/identification_validation/run_pathogensurveillance.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nextflow run ~/projects/pathogensurveillance/main.nf -profile docker -resume --input output/sample_metadata.tsv --reference_data output/reference_metadata.tsv --outdir output/pathsurveil_output --max_samples 120 --max_cpus 28 --cpu_scale 1 --max_ram 120
+nextflow run ~/projects/pathogensurveillance/main.nf -profile docker -resume --input output/sample_metadata.tsv --reference_data output/reference_metadata.tsv --outdir output/pathsurveil_output --max_samples 150 --max_cpus 28 --cpu_scale 1 --max_ram 120
